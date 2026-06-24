2e0f1ff39a306970a238a06c16b7e338db3261a1
info@birkeheeren.de <info@birkeheeren.de>
2026-06-24 04:16:50 +0200

    version 4.0 MIT licence

--- src/tonionlayout/LayoutMode.java
+++ src/tonionlayout/LayoutMode.java
@@ -1,4 +1,12 @@
-package tonionlayout;
+package vokabeltrainer.tonionlayout;
+
+/** LayoutMode 4.0 (released 24. June 2026)
+* 
+* Copyright (c) 2026 Birke Heeren
+*
+* Licensed under the MIT License.
+* 
+* */
 
 public enum LayoutMode
 {
